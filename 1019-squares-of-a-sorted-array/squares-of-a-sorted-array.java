81834f3a075c218d87aa25bbaa227a81f8491e9c
Aryawnnnn <aryanrajop18@gmail.com>
2026-08-22 00:27:08 +0530

    Time: 3 ms (43.10%) | Memory: 47.5 MB (43.44%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,21 +1,47 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
-        int k = nums.length - 1;
-
-        int[] res = new int[nums.length];
-        while(left<=right){
-            if(Math.abs(nums[left])>Math.abs(nums[right])){
-                res[k]=nums[left]*nums[left];
-                left++;
+        int n = nums.length;
+        List<Integer> pos =  new ArrayList<>();
+        List<Integer> neg =  new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                neg.add(nums[i]*nums[i]);
             }
             else{
-                res[k]=nums[right]*nums[right];
-                right--;
+                pos.add(nums[i]*nums[i]);
             }
-            k--;
         }
+        Collections.reverse(neg);
+
+        int[] res = new int[nums.length];
+
+        int i = 0;
+        int j=0;
+        int id=0;
+
+        while(i<neg.size()&&j<pos.size()){
+            if(neg.get(i)<=pos.get(j)){
+                res[id]=neg.get(i);
+                i++;
+            }
+            else{
+                res[id]=pos.get(j);
+                j++;
+            }
+            id++;
+        }
+        while (i < neg.size()) {
+            res[id] = neg.get(i);
+            i++;
+            id++;
+        }
+
+        while (j < pos.size()) {
+            res[id] = pos.get(j);
+            j++;
+            id++;
+        } 
         return res;
     }
 }
